955ab8e4b191ec0912d030d60aea1857ad8bfcc3
ahmadmadar <ahmadmadar501@gmail.com>
2018-01-09 09:50:44 -0700

    Create Walmart Greeter

--- CSharp Code Review/CodeReview/Program.cs
+++ CSharp Code Review/CodeReview/Program.cs
@@ -10,7 +10,12 @@ namespace CodeReview
     {
         static void Main(string[] args)
         {
+            // Use the salutation class for a walmart greeter
+            Salutation oldGuy = new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!");
 
+            // Get the guy to talk...
+            Console.WriteLine(oldGuy.Greet());
+            Console.WriteLine(oldGuy.SayFarewell());
         }
     }
 }
